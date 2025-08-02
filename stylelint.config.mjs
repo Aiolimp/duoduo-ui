43d3fae286d985b90bdbec989dcb12a4c1f16cad
’Aiolimp‘ <172978010@qq.com>
2025-08-03 00:51:12 +0800

    feat: ✨  delete some

--- stylelint.config.mjs
+++ stylelint.config.mjs
@@ -1,15 +1,10 @@
-/*
- *   Copyright (c) 2024 妙码学院-Heyi
- *   All rights reserved.
- *   妙码学院官方出品，作者 @Heyi，供学员学习使用，可用作练习，可用作美化简历，不可开源。
- */
 /** @type {import('stylelint').Config} */
 export default {
     // extends: ['stylelint-config-standard'],
     // customSyntax: 'postcss-styled-syntax',
     rules: {
         // 'unit-allowed-list': ['em'] // 只是测试使用em单位
-        'block-no-empty': true,
+        'block-no-empty': true
         // 'comment-empty-line-before': null,
         // 'selector-not-notation': null
     }
